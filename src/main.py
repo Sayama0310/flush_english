20a6d71e9f2c900e2c4b7e0c8415ca2444a45ca0
Sayama0310 <montblanc3060@gmail.com>
2023-02-28 20:48:55 +0900

    [feat] cursesによるhello world

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
-from dataset.problems import Problem, problems
+import curses
 
-if __name__ == "__main__":
-    for problem in problems:
-        print(problem.japanese)
-        print(problem.english)
-        print('----------------')
+# スクリーンの初期化
+screen = curses.initscr()
+
+# カーソルを非表示にする
+curses.curs_set(0)
+
+# 文字列を表示する座標を計算する
+x = screen.getmaxyx()[1] // 2 - len("Hello, World!") // 2
+y = screen.getmaxyx()[0] // 2
+
+# 文字列を表示する
+screen.addstr(y, x, "Hello, World!")
+
+# 変更を反映する
+screen.refresh()
+
+# キー入力待ち
+screen.getch()
+
+# スクリーンの終了処理
+curses.endwin()
